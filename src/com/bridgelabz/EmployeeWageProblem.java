edde763cf2cf9b51951e29dcf0087f2f0b9202e3
svs8 <surakshithvittalshetty@gmail.com>
2022-05-09 14:10:32 +0530

    Added:Calculate Wages till a condition of total working hours or days is reached for a month

--- src/com/bridgelabz/EmployeeWageProblem.java
+++ src/com/bridgelabz/EmployeeWageProblem.java
@@ -14,22 +14,27 @@ public class EmployeeWageProblem {
 
     private static final int TOTAL_WORKING_DAYS=20;
 
+    private static final int TOTAL_WORKING_HOURS=100;
+
     public static void main(String[] args)
         {
             System.out.println("Welcome to Employee Wage Computation Program on Master "
                     + "Branch  ");
-                int dailyWage=0;
-                int dayCount=0;
+                int workingHours=0;
+                int dayCount=1;
                 int totalWage=0;
-                while(dayCount<TOTAL_WORKING_DAYS) {
-                    int empStatus = (int) (Math.random() * 10) % 3;
+            while (dayCount <= TOTAL_WORKING_DAYS && workingHours <= TOTAL_WORKING_HOURS) {
+                int dailyWage=0;
+                int empStatus = (int) (Math.random() * 10) % 3;
                     switch (empStatus) {
                         case IS_FULL_TIME:
                             System.out.println("Employee worked FULL TIME");
+                            workingHours = workingHours + FULL_DAY_HOUR;
                             dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
                             break;
                         case IS_PART_TIME:
                             System.out.println("Employee worked PART_TIME");
+                            workingHours = workingHours + HALF_DAY_HOUR;
                             dailyWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
                             break;
                         default:
@@ -40,8 +45,9 @@ public class EmployeeWageProblem {
                     dayCount++;
                     totalWage = totalWage + dailyWage;
                 }
-
-            System.out.println(totalWage);
+            System.out.println("Total Days : "+ (dayCount-1));
+            System.out.println("Total Working Hours : " + workingHours);
+            System.out.println("Total wage for a month :" +totalWage);
 
             }
 
