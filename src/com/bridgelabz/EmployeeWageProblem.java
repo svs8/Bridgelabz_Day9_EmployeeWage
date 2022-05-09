011009cab931a858ea6f29ac441dcfb24273fff9
svs8 <surakshithvittalshetty@gmail.com>
2022-05-09 12:46:25 +0530

    Added:Logic to check whether the employee is present or not

--- src/com/bridgelabz/EmployeeWageProblem.java
+++ src/com/bridgelabz/EmployeeWageProblem.java
@@ -7,7 +7,21 @@ public class EmployeeWageProblem {
             System.out.println("Welcome to Employee Wage Computation Program on Master "
                     + "Branch  ");
 
+                int empPresent = 1;
+                int empStatus = (int) (Math.random() * 10) % 2;
+                System.out.println(empStatus);
+                if (empStatus == empPresent)
+                {
+                    System.out.println("Emp is Present ");
+                }
+                else
+                {
+                    System.out.println("Emp is not Present ");
+                }
+            }
+
         }
-    }
+
+
 
 
