f33dd2d1d78056ca8ca18faad9d0be2e1cf53151
svs8 <surakshithvittalshetty@gmail.com>
2022-05-09 13:51:44 +0530

    Added:Calculating Wages for a month

--- src/com/bridgelabz/EmployeeWageProblem.java
+++ src/com/bridgelabz/EmployeeWageProblem.java
@@ -12,26 +12,37 @@ public class EmployeeWageProblem {
 
     private static final int WAGE_PER_HOUR = 20;
 
+    private static final int TOTAL_WORKING_DAYS=20;
+
     public static void main(String[] args)
         {
             System.out.println("Welcome to Employee Wage Computation Program on Master "
                     + "Branch  ");
                 int dailyWage=0;
-                int empStatus = (int) (Math.random() * 10) % 3;
-                System.out.println(empStatus);
-            switch (empStatus) {
-                case IS_FULL_TIME:
-                    System.out.println("Employee worked FULL TIME");
-                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                    break;
-                case IS_PART_TIME:
-                    System.out.println("Employee worked PART_TIME");
-                    dailyWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
-                    break;
-                default:
-                    System.out.println("Employee is Absent");
-            }
-            System.out.println("Daily wage:"+dailyWage);
+                int dayCount=0;
+                int totalWage=0;
+                while(dayCount<TOTAL_WORKING_DAYS) {
+                    int empStatus = (int) (Math.random() * 10) % 3;
+                    switch (empStatus) {
+                        case IS_FULL_TIME:
+                            System.out.println("Employee worked FULL TIME");
+                            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                            break;
+                        case IS_PART_TIME:
+                            System.out.println("Employee worked PART_TIME");
+                            dailyWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
+                            break;
+                        default:
+                            System.out.println("Employee is Absent");
+                            dailyWage=0;
+                    }
+                    System.out.println("Daily Wage for day : " + dayCount + " is " + dailyWage);
+                    dayCount++;
+                    totalWage = totalWage + dailyWage;
+                }
+
+            System.out.println(totalWage);
+
             }
 
         }
