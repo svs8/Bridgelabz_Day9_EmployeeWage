d36ce78042e98e625e2d4a65122cab6650ff2e63
svs8 <surakshithvittalshetty@gmail.com>
2022-05-09 13:27:03 +0530

    Added:Logic to check employee wage for part time and full time

--- src/com/bridgelabz/EmployeeWageProblem.java
+++ src/com/bridgelabz/EmployeeWageProblem.java
@@ -4,7 +4,11 @@ public class EmployeeWageProblem {
 
     private static final int FULL_DAY_HOUR = 10;
 
+    private static final int HALF_DAY_HOUR = 5;
+
     private static final int IS_FULL_TIME = 1;
+
+    private static final int IS_PART_TIME = 2;
 
     private static final int WAGE_PER_HOUR = 20;
 
@@ -13,23 +17,22 @@ public class EmployeeWageProblem {
             System.out.println("Welcome to Employee Wage Computation Program on Master "
                     + "Branch  ");
                 int dailyWage=0;
-                int empPresent = 1;
                 int workingHours = 0;
-                int totalWage = 0;
-                int empStatus = (int) (Math.random() * 10) % 2;
+                int empStatus = (int) (Math.random() * 10) % 3;
                 System.out.println(empStatus);
-                if (empStatus == empPresent)
-                {
-                    System.out.println("Emp is Present ");
-                    if(empStatus==IS_FULL_TIME) {
-                        System.out.println("Emp Worked Full time");
-                        dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
-                    }
-                }
-                else
-                {
-                    System.out.println("Emp is not Present ");
-                }
+            switch (empStatus) {
+                case IS_FULL_TIME:
+                    System.out.println("Employee worked FULL TIME");
+                    //workingHours = workingHours + FULL_DAY_HOUR;
+                    dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    break;
+                case IS_PART_TIME:
+                    System.out.println("Employee worked PART_TIME");
+                    dailyWage = WAGE_PER_HOUR * HALF_DAY_HOUR;
+                    break;
+                default:
+                    System.out.println("Employee is Absent");
+            }
             System.out.println("Daily wage:"+dailyWage);
             }
 
