ff259c1706d542887e70101d7f5288ec5a105dd5
svs8 <surakshithvittalshetty@gmail.com>
2022-05-09 12:36:25 +0530

    Added:Message

---
+++ README.md
@@ -0,0 +1,1 @@
+# Bridgelabz_Day9_EmployeeWage
---
+++ src/com/bridgelabz/EmployeeWageProblem.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class EmployeeWageProblem {
+
+        public static void main(String[] args)
+        {
+            System.out.println("Welcome to Employee Wage Computation Program on Master "
+                    + "Branch  ");
+
+        }
+    }
+
+
