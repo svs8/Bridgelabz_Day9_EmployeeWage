84f8a3f325ff208102b24d9ec2328f62431e6564
svs8 <surakshithvittalshetty@gmail.com>
2022-05-09 13:16:29 +0530

    Added:logic to check employee wage

--- src/com/bridgelabz/EmployeeWageProblem.java
+++ src/com/bridgelabz/EmployeeWageProblem.java
@@ -2,22 +2,35 @@ package com.bridgelabz;
 
 public class EmployeeWageProblem {
 
-        public static void main(String[] args)
+    private static final int FULL_DAY_HOUR = 10;
+
+    private static final int IS_FULL_TIME = 1;
+
+    private static final int WAGE_PER_HOUR = 20;
+
+    public static void main(String[] args)
         {
             System.out.println("Welcome to Employee Wage Computation Program on Master "
                     + "Branch  ");
-
+                int dailyWage=0;
                 int empPresent = 1;
+                int workingHours = 0;
+                int totalWage = 0;
                 int empStatus = (int) (Math.random() * 10) % 2;
                 System.out.println(empStatus);
                 if (empStatus == empPresent)
                 {
                     System.out.println("Emp is Present ");
+                    if(empStatus==IS_FULL_TIME) {
+                        System.out.println("Emp Worked Full time");
+                        dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+                    }
                 }
                 else
                 {
                     System.out.println("Emp is not Present ");
                 }
+            System.out.println("Daily wage:"+dailyWage);
             }
 
         }
